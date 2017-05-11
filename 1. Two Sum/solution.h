f235b707e508925d51532222ab24646d4420ce3f
李源 <419924249@qq.com>
2017-05-11 22:03:49 +0800

    Create solution.h

---
+++ 1. Two Sum/solution.h
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		int i, j;
+		vector<int> vec;
+		for (i = 0; i<nums.size(); i++)
+			for (j = i + 1; j<nums.size(); j++)
+				if (nums[i] + nums[j] == target) {
+					vec.push_back(i);
+					vec.push_back(j);
+					return vec;
+				}
+		return vec;
+	}
+};
