40a37f3488f837b7da832a1ecfd3f94f733f67e2
李源 <419924249@qq.com>
2017-05-11 22:26:13 +0800

    Update main.cpp

--- 1. Two Sum/main.cpp
+++ 1. Two Sum/main.cpp
@@ -1,13 +1,11 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+#include"solution.h"
 
 int main()
 {
 	Solution sol;
 	vector<int> nums = { 2, 7, 11, 15 };
-	vector<int> vec = sol.twoSum(nums, 16);
-	if(vec.size()==2)
+	vector<int> vec = sol.twoSum(nums, 26);
+	if (vec.size() == 2)
 		cout << '[' << vec[0] << ',' << vec[1] << ']' << endl;
 	return 0;
 }
