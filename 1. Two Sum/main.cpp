55bcbc38c3b76e243e024ef700fc37d3444ed98b
李源 <419924249@qq.com>
2017-05-11 22:06:00 +0800

    Update main.cpp

--- 1. Two Sum/main.cpp
+++ 1. Two Sum/main.cpp
@@ -1,21 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-class Solution {
-public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		int i, j;
-		vector<int> vec;
-		for (i = 0; i<nums.size(); i++)
-			for (j = i + 1; j<nums.size(); j++)
-				if (nums[i] + nums[j] == target) {
-					vec.push_back(i);
-					vec.push_back(j);
-					return vec;
-				}
-		return vec;
-	}
-};
+
 int main()
 {
 	Solution sol;
