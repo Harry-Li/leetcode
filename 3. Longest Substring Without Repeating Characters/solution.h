6b1d8ba1c4566a6d67048d9a3e2830e70be163a3
李源 <419924249@qq.com>
2017-05-13 21:25:40 +0800

    Create solution.h

---
+++ 3. Longest Substring Without Repeating Characters/solution.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<algorithm>
+using namespace std;
+int lengthOfLongestSubstring(string s) {
+	map<char, int> charMap;
+	int start = -1;
+	int maxLen = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (charMap.count(s[i]) != 0) {
+			start = max(start, charMap[s[i]]);
+		}
+		charMap[s[i]] = i;
+		maxLen = max(maxLen, i - start);
+	}
+	return maxLen;
+}
