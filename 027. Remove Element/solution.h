bfa65bff694abbe053bf9a9fa2ae7d36efebed09
李源 <419924249@qq.com>
2017-06-01 10:44:57 +0800

    Create solution.h

---
+++ 027. Remove Element/solution.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		if (nums.empty()) return 0;
+		int i = 0;
+		for (int j = 0; j<nums.size(); j++) {
+			if (nums[j] != val) {
+				nums[i] = nums[j];
+				++i;
+			}
+		}
+		return i;
+	}
+};
