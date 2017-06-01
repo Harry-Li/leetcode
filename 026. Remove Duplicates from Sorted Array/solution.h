433b16754a7826d98e84b380090ba5535ceb077c
李源 <419924249@qq.com>
2017-06-01 09:56:10 +0800

    Create solution.h

---
+++ 026. Remove Duplicates from Sorted Array/solution.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		if (nums.empty()) return 0;
+		int i = 1;
+		for (int j = 1; j<nums.size(); j++) {
+			if (nums[j] != nums[j - 1]) {
+				nums[i++] = nums[j];
+			}
+		}
+		return i;
+	}
+};
